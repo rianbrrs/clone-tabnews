499fda12f740d9bd449e6457e4d782a8b76d8cbf
Rian Barros <108371546+rianbrrs@users.noreply.github.com>
2024-11-28 02:50:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Sarah, Você é um pedaço em mim. Eu quero viver em seus braços para sempre! ❤️</h1>
+  return (
+    <h1>
+      Sarah, Você é um pedaço em mim. Eu quero viver em seus braços para sempre!
+      ❤️
+    </h1>
+  );
 }
 
 export default Home;
